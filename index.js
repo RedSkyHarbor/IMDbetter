d6c70a0bad0f2abea039c4dd7b1c57e7318fa4e9
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-21 22:14:26 -0400

    renamed '/' endpoint to '/hello' and required path to fix error

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
+
+const app = express();
 
 // Serve static files from the React application
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // API endpoints
-app.get('/', (req, res) => {
-	res.json({ info: 'Hello World!'})
+app.get('/hello', (req, res) => {
+	res.json({ info: 'Hello World!' });
 });
 
 // Any request that matches none of the above endpoints returns React application's index page
