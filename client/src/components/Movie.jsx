a726ee88a79a0f323e01097a2dd588213a2266e0
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 04:00:30 -0400

    create Movie prop

---
+++ client/src/components/Movie.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+    state = { data: [] };
+    
+
+    componentDidMount(){
+        let movieId = this.props.movieId;
+        console.log(movieId);
+        fetch('/api/movie/' + movieId)
+            .then(response => response.json())
+                .then(data => this.setState({ data }))
+    }
+
+    render() { 
+        return (
+            <>
+            {this.state.data.map(d =>
+                <div key={d.id}>
+                    <h1>Title: {d.title}</h1>
+                    <p>Description: {d.description}</p>
+                    <p>Rating: {d.rating}</p>
+                    <p>Release in: {d.release_year}</p>    
+                </div>  
+            )}
+            </>
+        );
+    }
+}
+
+export default Movie;
