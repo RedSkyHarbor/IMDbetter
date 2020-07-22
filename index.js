3b36efee2052be6bdf7607a95706743ed8019418
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-21 23:30:38 -0400

    created endpoint that returns all movies in db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { pool } = require('./config');
 const port = 5000;
 
 const app = express();
@@ -7,16 +8,31 @@ const app = express();
 // Serve static files from the React application
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// API endpoints
+// Testing endpoint
 app.get('/hello', (req, res) => {
 	res.json({ info: 'Hello World!' });
 });
 
-// Any request that matches none of the above endpoints returns React application's index page
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+// API endpoints
+const getMovies = (request, response) => {
+	pool.query('SELECT * from movies', (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
+}
 
+// Any request that matches none of the above endpoints returns React application's index page
+//app.get('*', (req, res) => {
+//	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+//});
+
+app.route('/api')
+	.get(getMovies)
+
+
+// Starts server
 app.listen(port, () => {
 	console.log(`Express server is listening on port ${port}`);
 });
