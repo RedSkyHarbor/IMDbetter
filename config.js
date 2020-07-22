c0f62da6d3e3fc2b474aa54aee33adf96489a3aa
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 17:37:56 -0400

    added logging statement to see if production env variables are present

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const { Pool } = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
+const { Pool } = require('pg');
+const isProduction = process.env.NODE_ENV === 'production';
 
-const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+console.log('is this production? ', isProduction);
+
+const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
-})
+});
 
-module.exports = { pool }
+module.exports = { pool };
