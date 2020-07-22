9a5147defe6bcba76c0bb92d30687968844fbb39
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 01:10:05 -0400

    Create HomePage component

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AllMovies from '../components/AllMovies';
+
+const HomePage = () => {
+    return(
+        <div>
+            HomePage
+            <AllMovies />
+        </div>
+    );
+}
+
+export default HomePage;
