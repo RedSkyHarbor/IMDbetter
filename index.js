85d5f293b1bb40c3ee826334f65addc4ec9cf15b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 16:47:49 -0400

    hardcoded port makes deploying to heroku not work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { pool } = require('./config');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
