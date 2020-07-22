66d435f6c0c23626126ae26fa2cd0cf4efecfc56
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 17:18:27 -0400

    fixed silly mistake

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 const { pool } = require('./config');
 const port = process.env.PORT || 5000;
