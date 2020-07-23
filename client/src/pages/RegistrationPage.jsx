94fc49e4115b4e45e7a4c1259a2db1f9fcf26236
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 22:36:19 -0400

    created registration page

---
+++ client/src/pages/RegistrationPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../components/header/Header';
+
+const RegistrationPage = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Sign in</h1>  
+            <p>Sign in with IMDbetter</p>
+            <p>Create New Account</p>  
+        </div>
+    );
+}
+
+export default RegistrationPage;
