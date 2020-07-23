e1157f148d601b87d0e06c4de66c481b0ec63748
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 22:35:49 -0400

    added text input to query for movies by their partial title

--- client/src/components/AllMovies.jsx
+++ client/src/components/AllMovies.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class AllMovies extends Component {
-    state = { movies: [] }
+    constructor(props) {
+        super(props);
+        this.state = { movies: [] };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
     componentDidMount() {
         fetch('/api')
@@ -10,23 +14,40 @@ class AllMovies extends Component {
                 .then(movies => this.setState({ movies }));
     }
 
+    handleChange(event) {
+        /* Handles partial searches */
+        let { value } = event.target;
+        if (value.length === 0){ 
+            fetch('/api')
+            .then(response => response.json())
+                .then(movies => this.setState({ movies }));
+        } else if (value.length > 0) {
+            fetch('/api/fuzzysearch/' + value)
+                .then(res => res.json())
+                    .then(movies => this.setState({ movies }));
+        }
+    }
+
     render() {
         const { movies } = this.state;
         return (
-            <ul>
-                { movies.map(movie =>
-                    <li key={movie.id}>
-                        <h1><Link to={{
-                            pathname: `/movie/${movie.slug}`,
-                            state: { movieId: movie.id }
-                        }}>{movie.title}</Link></h1>
-                        <img alt='movie poster' style={{ width: 200, height: 300 }} src={movie.picture_url} />
-                        <p>{movie.summary}</p>
-                        <p>{movie.rating}</p>
-                        <p>{movie.release_year}</p>
-                    </li>
-                )}
-            </ul>
+            <div>
+                <input type='text' placeholder='Search IMDbetter' onChange={this.handleChange} />
+                <ul>
+                    { movies.map(movie =>
+                        <li key={movie.id}>
+                            <h1><Link to={{
+                                pathname: `/movie/${movie.slug}`,
+                                state: { movieId: movie.id }
+                            }}>{movie.title}</Link></h1>
+                            <img alt='movie poster' style={{ width: 200, height: 300 }} src={movie.picture_url} />
+                            <p>{movie.summary}</p>
+                            <p>{movie.rating}</p>
+                            <p>{movie.release_year}</p>
+                        </li>
+                    )}
+                </ul>
+            </div>
         );
     }
 }
