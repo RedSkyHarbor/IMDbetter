da82f67a8cbe09fb9a4754f931760990a2d63b5c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-23 14:41:19 -0400

    admin login page created

---
+++ client/src/pages/AdminLoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/header/Header';
+import AdminLogin from '../components/auth/AdminLogin';
+
+const AdminLoginPage = () => {
+    return (
+        <div>
+            <Header />
+            <AdminLogin />
+        </div>
+    )
+}
+
+export default AdminLoginPage;
