83138507359a81783e05c08b5f11f96d12d656c3
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-22 22:37:06 -0400

    removed search bar from header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = { searchQuery: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    componentDidMount() {
-
-    }
-
-    handleChange(event) {
-        console.log(event.target.value);
-        this.setState({ searchQuery: event.target.value });
-    }
-
-    handleSubmit(event) {
-        alert(this.state.searchQuery);
-        event.preventDefault();
+        this.state = { };
     }
 
     render() {
         return (
             <header>
                 <Link to='/' >IMDbetter</Link>
-                <form onSubmit={this.handleSubmit}>
-                    <input type='text' placeholder='Search IMDbetter' onChange={this.handleChange} />
-                    <input type='submit' value='Submit'/>
-                </form>
+                <br />
+                <Link to='/registration'>Log in</Link>
             </header>
         );
     }
