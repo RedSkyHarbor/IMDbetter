7aac9ba9167dcdf5571f47afd26bc7153a2a2339
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-24 18:59:38 -0400

    refactor AdminLoginPage -> AdminAuthPage

--- client/src/pages/AdminAuthPage.jsx
+++ client/src/pages/AdminAuthPage.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 import AdminLogin from '../components/auth/AdminLogin';
 
-const AdminLoginPage = () => {
+const AdminLoginPage = (props) => {
     return (
         <div>
+            <h1>{props.loggedInStatus}</h1>
             <AdminLogin />
         </div>
     )
