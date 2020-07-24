64396c4f10be7163368a59387cc265bf44045e6c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-24 18:04:36 -0400

    migrated db - testing heroku again

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const connectionString = `postgresql://${process.env.DB_USER}@${process.env.DB_H
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  // for prod v
+  // for production v
   ssl: { rejectUnauthorized: false}
   // for local v
-  //ssl: isProduction
+  //ssl: false
 });
 
 module.exports = { pool };
