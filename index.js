7feba0ba6dce1471cc327a58ca1564838d6bb1dd
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-24 22:25:32 -0400

    maybe i should point to the index file in my public folder - though heroku SHOULD be using my build folder. Maybe build folder is right but it currently isnt tracked by git so the path is failing?

--- index.js
+++ index.js
@@ -145,6 +145,14 @@ app.get('*', (req, res) => {
 })
 */
 
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname, '/client/public/index.html'), function(err) {
+	  if (err) {
+		res.status(500).send(err)
+	  }
+	})
+  })
+
 app.route('/api')
 	.get(getAllMovies)
 
