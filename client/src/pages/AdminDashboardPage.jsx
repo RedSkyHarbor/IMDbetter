b4351873ad469d9ef45e1b2227b13b216bc93351
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-25 17:47:58 -0400

    added post request to create movie

--- client/src/pages/AdminDashboardPage.jsx
+++ client/src/pages/AdminDashboardPage.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+//TODO only accessible with admin cookie MUST BE LOCKED BEHIND THAT LOGIN
+//send data to backend with axios
 class AdminDashboardPage extends Component {
     constructor(props){
         super(props);
         this.state = {
             title: '',
             summary: '',
-            image_url: ''
+            image_url: '',
+            response: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,6 +20,25 @@ class AdminDashboardPage extends Component {
     handleSubmit = (event) => {
         const { title, summary, image_url } = this.state;
         console.log('handle submit', title, summary, image_url);
+        axios  
+            .post('/api/insert_movie', {
+                movie: {
+                    title: title,
+                    summary: summary,
+                    image_url: image_url
+                }
+            },
+            { withCredentials: true}
+        )
+        .then(res => {
+            if (res.status === 204) {
+                this.setState({ response: 'Movie inserted successfully'})
+            }
+        })
+        .catch(err => {
+            console.log('insert movie err', err);
+            this.setState({ response: 'Error while inserting movie' })
+        })
         event.preventDefault();
     }
 
@@ -27,8 +49,10 @@ class AdminDashboardPage extends Component {
     }
 
     render () {
+        let { response } = this.state;
         return (
             <div>
+                <h2>{response}</h2>
                 <form onSubmit={this.handleSubmit}>
                     <li>
                         <label htmlFor='title'>Title</label>
