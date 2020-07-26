108147c62fca9c807a594e8f2b3ab1b5e9f0a69a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-26 03:55:21 -0400

    alter db schema

--- init.sql
+++ init.sql
@@ -8,22 +8,10 @@ CREATE TABLE movies (
 	title VARCHAR(255) NOT NULL,
 	slug VARCHAR(255) NOT NULL,
 	summary VARCHAR(511) NOT NULL,
-	rating DOUBLE PRECISION NOT NULL,
 	release_year INTEGER NOT NULL,
+	avg_rating DOUBLE PRECISION DEFAULT NULL,
 	picture_url VARCHAR(255) NOT NULL
 );
-
-INSERT INTO movies (title, slug, summary, rating, release_year, picture_url)
-VALUES ('Spaceballs', 'Spaceballs', 'A star pilot and his sidekick must come to the rescue of a Princess and save the galaxy from a ruthless race of beings known as Spaceballs.', 7.1, 1987, 'https://i.imgur.com/WJ4IcNG.jpg');
-
-INSERT INTO movies (title, slug, summary, rating, release_year, picture_url)
-VALUES ('Young Frankenstein', 'Young-Frankenstein', 'An American grandson of the infamous scientist, struggling to prove that his grandfather was not as insane as people believe, is invited to Transylvania, where he discovers the process that reanimates a dead body.', 8.0, 1974, 'https://i.imgur.com/hOg8EGj.jpg');
-
-INSERT INTO movies (title, slug, summary, rating, release_year, picture_url)
-VALUES ('Robin Hood: Men in Tights', 'Robin-Hood-Men-In-Tights', 'A spoof of Robin Hood in general, and Robin Hood: Prince of Thieves (1991) in particular.', 6.7, 1993, 'https://i.imgur.com/xe9mRUm.jpg');
-
-INSERT INTO movies (title, slug, summary, rating, release_year, picture_url)
-VALUES ('The Producers', 'The-Producers', 'A stage-play producer devises a plan to make money by producing a sure-fire flop.', 7.6, 1967, 'https://i.imgur.com/twgL1fA.jpg');
 
 /* Users */ 
 CREATE TABLE users (
@@ -34,12 +22,7 @@ CREATE TABLE users (
 	is_admin BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO users (uname, pword, email)
-VALUES ('Jesse', 'jesse123', 'PenelloJ2@gmail.com');
-
-INSERT INTO users (uname, pword, email, is_admin)
-VALUES ('TechFlex', 'adminaccount', 'admin@TechFlex.com', TRUE);
-
+/* comments */
 CREATE TABLE comments (
 	ID SERIAL PRIMARY KEY,
 	movieID INTEGER REFERENCES movies(ID) NOT NULL,
@@ -48,14 +31,64 @@ CREATE TABLE comments (
 	rating DOUBLE PRECISION NOT NULL
 );
 
-INSERT INTO comments (movieID, userID, comment, rating, created)
-VALUES (1, 1, 'One of my favorites - better than star wars itself', 9.0, current_timestamp);
+/* movie ratings */
+CREATE TABLE movie_ratings (
+	ID SERIAL PRIMARY KEY,
+	movieID INTEGER REFERENCES movies(ID) NOT NULL,
+	userID INTEGER REFERENCES users(ID) NOT NULL,
+	rating DOUBLE PRECISION NOT NULL
+);
 
-INSERT INTO comments (movieID, userID, comment, rating, created)
-VALUES (2, 1, 'A funny remake of the classic story of Frankenstein', 8.5, current_timestamp);
+/* Users */ 
+INSERT INTO users (uname, pword, email) VALUES ('Jesse', 'jesse123', 'PenelloJ2@gmail.com');
+INSERT INTO users (uname, pword, email) VALUES ('Steez', 'steez123', 'Jesse@codesteez.com');
+INSERT INTO users (uname, pword, email, is_admin) VALUES ('admin', 'admin', 'admin@gmail.com', TRUE);
 
-INSERT INTO comments (movieID, userID, comment, rating, created)
-VALUES (3, 1, 'Not the best but still better then twilight', 6.5, current_timestamp);
+/* Movies */ 
+INSERT INTO movies (title, slug, summary, release_year, picture_url)
+VALUES ('Spaceballs', 'Spaceballs', 'A star pilot and his sidekick must come to the rescue of a Princess and save the galaxy from a ruthless race of beings known as Spaceballs.', 1987, 'https://i.imgur.com/WJ4IcNG.jpg');
 
-INSERT INTO comments (movieID, userID, comment, rating, created)
-VALUES (4, 1, 'I prefer the remake that came out in 2005, still funny though', 6, current_timestamp);
+INSERT INTO movies (title, slug, summary, release_year, picture_url)
+VALUES ('Young Frankenstein', 'Young-Frankenstein', 'An American grandson of the infamous scientist, struggling to prove that his grandfather was not as insane as people believe, is invited to Transylvania, where he discovers the process that reanimates a dead body.', 1974, 'https://i.imgur.com/hOg8EGj.jpg');
+
+INSERT INTO movies (title, slug, summary, release_year, picture_url)
+VALUES ('Robin Hood: Men in Tights', 'Robin-Hood-Men-In-Tights', 'A spoof of Robin Hood in general, and Robin Hood: Prince of Thieves (1991) in particular.', 1993, 'https://i.imgur.com/xe9mRUm.jpg');
+
+INSERT INTO movies (title, slug, summary, release_year, picture_url)
+VALUES ('The Producers', 'The-Producers', 'A stage-play producer devises a plan to make money by producing a sure-fire flop.', 1967, 'https://i.imgur.com/twgL1fA.jpg');
+
+/* comments */
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (1, 1, 'One of my favorites - better than star wars itself', 9.0);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (1, 2, 'Very quotable, very funny', 10.0);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (2, 1, 'A funny remake of the classic story of Frankenstein', 8.0);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (2, 2, 'Havent seen it in a while, but very silly', 7.0);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (3, 1, 'Not the best but still better then twilight', 6.0);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (3, 2, 'Nonstop jokes, another classic spoof movie', 7.0);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (4, 1, 'I prefer the remake that came out in 2005, still funny though', 6);
+
+INSERT INTO comments (movieID, userID, comment, rating)
+VALUES (4, 2, 'Not very good', 2.0);
+
+/* ratings */
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(1, 1, 9.0);
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(2, 1, 8.0);
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(3, 1, 6.0);
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(4, 1, 6.0);
+
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(1, 2, 10.0);
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(2, 2, 7.0);
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(3, 2, 7.0);
+INSERT INTO movie_ratings (movieID, userID, rating) VALUES(4, 2, 2.0);
