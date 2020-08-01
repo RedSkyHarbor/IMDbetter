fadbd0cc4cf8638fc0e5b7db9b406c9b8d0c90df
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-01 19:22:06 -0400

    removed comments

--- client/src/pages/MoviePage.jsx
+++ client/src/pages/MoviePage.jsx
@@ -5,65 +5,13 @@ import Movie from '../components/Movie';
 import UserReviews from '../components/UserReviews';
 import './pages.scss'
 
-// TODO check if reviewed already here
 class MoviePage extends Component {
     constructor(props) {
         super(props);
         this.state = {};
-        //this.getReview = this.getReview.bind(this);
-        //this.updateReview = this.updateReview.bind(this);
-        //this.checkIfFirstReview = this.checkIfFirstReview.bind(this);
     }
     
-    /*
-    componentDidMount(){
-        if (this.props.loggedInStatus === 'LOGGED_IN') {
-            this.checkIfFirstReview();
-        }
-    }
 
-    checkIfFirstReview = () => {
-        axios
-            .post('/api/review/check', {
-                review: {
-                    movieId: localStorage.getItem('movieId')
-                }
-            },
-            { withCredentials: true }
-        )
-        .then(res => {
-            console.log(res);
-            this.setState({
-                num_reviews: res.data
-            })
-        })
-        .catch(err => {
-            console.log('checkIfFirstReview', err);
-        })
-    }
-
-    getReview = () => {
-        console.log('getReview called');
-        axios
-            .post('/api/review/get_review', {
-                comment: {
-                    movieId: localStorage.getItem('movieId')
-                }
-            },
-            { withCredentials: true }
-        )
-        .then(res => {
-            console.log(res);
-        })
-        .catch(err => {
-            console.log('getReview error', err);
-        })
-    }
-
-    updateReview = () => {
-
-    }
-    */
 
     render() {
         let movieId = localStorage.getItem('movieId');
